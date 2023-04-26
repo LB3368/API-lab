77e695fdc0939d41629a827014d017ae4518d382
Latisha Bridges <msbridges3368@gmail.com>
2023-04-26 17:34:03 -0400

    first commit

---
+++ Part1.js
@@ -0,0 +1,97 @@
+//the height of Darth Vader
+//Param: height (res.body.height)
+//https://swapi.dev/api/people/?search=Darth
+/**
+ *       "name": "Darth Vader",
+      "height": "202",
+      "mass": "136",
+      "hair_color": "none",
+      "skin_color": "white",
+      "eye_color": "yellow",
+      "birth_year": "41.9BBY",
+      "gender": "male",
+      "homeworld": "https://swapi.dev/api/planets/1/",
+      "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      "species": [],
+      "vehicles": [],
+      "starships": [
+        "https://swapi.dev/api/starships/13/"
+      ],
+      "created": "2014-12-10T15:18:20.704000Z",
+      "edited": "2014-12-20T21:17:50.313000Z",
+      "url": "https://swapi.dev/api/people/4/"
+ */
+
+      //The population of the planet Alderaan
+      //Param: population (res.body.population)
+      /**
+       *  {
+      "name": "Alderaan",
+      "rotation_period": "24",
+      "orbital_period": "364",
+      "diameter": "12500",
+      "climate": "temperate",
+      "gravity": "1 standard",
+      "terrain": "grasslands, mountains",
+      "surface_water": "40",
+      "population": "2000000000",
+      "residents": [
+       */
+
+        //name of the manufacturer of the Millennium Falcon
+        //Param: manufacturer
+        /* "name": "Millennium Falcon",
+      "model": "YT-1300 light freighter",
+      "manufacturer": "Corellian Engineering Corporation",
+      "cost_in_credits": "100000",*/
+
+      //The name of the species that C-3PO belongs to
+      //https://swapi.dev/api/people/?search=c-3
+      //https://swapi.dev/api/species/2/
+      //Param: species
+      /**
+       * "name": "Droid",
+  "classification": "artificial",
+  "designation": "sentient",
+  "average_height": "n/a",
+  "skin_colors": "n/a",
+  "hair_colors": "n/a",
+  "eye_colors": "n/a",
+  "average_lifespan": "indefinite",
+  "homeworld": null,
+  "language": "n/a",
+  "people": [
+    "https://swapi.dev/api/people/2/",
+    "https://swapi.dev/api/people/3/",
+    "https://swapi.dev/api/people/8/",
+    "https://swapi.dev/api/people/23/"
+       */
+
+    //the title of each film that Obi-Wan Kenobi is in (multiple URLs)
+    //Param: url
+//https://swapi.dev/api/people/?search=obi
+/**
+ * search character of film Param: character 
+ * https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+ */
+// A New Hope
+// Clone Wars
+//Revenge of the Sith
+//Phantom Menace
+//Attack of the Clones
+//Return of the Jedi
+
+
+
+//use the search query (the how to on the search query is at the bottom of the Getting Started section of the documentation) to get the information about the Millennium Falcon, it’s a starship
+//https://swapi.dev/api/starships/?search=mil
---
+++ Part2.js
@@ -0,0 +1,39 @@
+//Check if the POST request accept params, queries, and/or a body. Which one(s) and what information is it expecting to be sent?
+
+//https://practiceapi.devmountain.com/api/get
+//https://practiceapi.devmountain.com/api/post
+
+//What data type does the GET request return?
+
+//What would the URL look like for deleting the post with the id 555? (This post does not exist anymore, but the syntax is the same for existing posts, )
+//**doesn't return anything */
+
+//List the possible response codes from the GET request at ‘/posts/filter’
+
+//status 200 ok
+
+//Create a post whose text is your name, record the URL and body here:
+//https://web.postman.co/workspace/DevMountain~7b825130-3874-4c11-a042-51fbba643754/request/20531299-fa1d72c0-fc18-4a5d-8ff9-48fd25fa4cd4
+/*{
+    "id": 2801,
+    "text": "Ms. Bridges",
+    "date": "26 Apr 2023"
+}*/
+
+
+//What would the URL and body object be to update the post you just made to contain your faovrite color instead of your name?
+//https://web.postman.co/workspace/DevMountain~7b825130-3874-4c11-a042-51fbba643754/request/20531299-fa1d72c0-fc18-4a5d-8ff9-48fd25fa4cd4
+
+//What is the URL to get posts that contain the text “blue”?
+//https://web.postman.co/workspace/DevMountain~7b825130-3874-4c11-a042-51fbba643754/request/20531299-fa1d72c0-fc18-4a5d-8ff9-48fd25fa4cd4
+
+//Make a request to GET all the posts. What are the content type and charset of the response? (Hint: look on the Headers)
+//https://web.postman.co/workspace/DevMountain~7b825130-3874-4c11-a042-51fbba643754/request/20531299-fa1d72c0-fc18-4a5d-8ff9-48fd25fa4cd4
+//[{"key":"X-Powered-By","value":"Express","description":"Specifies the technology (ASP.NET, PHP, JBoss, e.g.) supporting the web application.","enabled":true,"disableEdit":true,"info":"Specifies the technology (ASP.NET, PHP, JBoss, e.g.) supporting the web application."},{"key":"Access-Control-Allow-Origin","value":"*","description":"Specifies a URI that may access the response.","enabled":true,"disableEdit":true,"info":"Specifies a URI that may access the response."},{"key":"ETag","value":"W/\"2ca0-SmGYW+x+YNbqGtdkfmodmBQl4kQ\"","description":"Identifies a specific resource version.","enabled":true,"disableEdit":true,"info":"Identifies a specific resource version."},{"key":"CF-Cache-Status","value":"DYNAMIC","description":"Custom header","enabled":true,"disableEdit":true,"info":"Custom header"},{"key":"Strict-Transport-Security","value":"max-age=15552000","description":"Lets websites tell a browser that they should only be accessed via HTTPS.","enabled":true,"disableEdit":true,"info":"Lets websites tell a browser that they should only be accessed via HTTPS."},{"key":"Server","value":"cloudflare","description":"Contains information about how the server handles requests.","enabled":true,"disableEdit":true,"info":"Contains information about how the server handles requests."},{"key":"CF-RAY","value":"7be1e6b59cd09c70-IAD","description":"Custom header","enabled":true,"disableEdit":true,"info":"Custom header"},{"key":"Content-Encoding","value":"gzip","description":"An entity header indicating the encoding method applied to the entity body.","enabled":true,"disableEdit":true,"info":"An entity header indicating the encoding method applied to the entity body."}]
+
+//What would cause a PUT request to return a 409 status?
+//not entering the params
+
+//What happens if you try to send a query in the GET request URL? Why do you get that response?
+// It gave me the information that I queried.
+//https://web.postman.co/workspace/DevMountain~7b825130-3874-4c11-a042-51fbba643754/request/20531299-fa1d72c0-fc18-4a5d-8ff9-48fd25fa4cd4
